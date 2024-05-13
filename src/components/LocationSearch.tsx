d997e006337891c6295a46b5f50b54386e04a6b7
Anja Petry <anjapetry@protonmail.com>
2024-05-13 20:17:03 +0200

    feat: create search form in locationsearch

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,7 +1,35 @@
 import type { Place } from "../api/Place";
+import { useState } from "react";
 
-const LocationSearch = () => {
-    return <div>Search for a location!</div>;
+interface LocationSearchProps {
+    onPlaceClick: (place: Place) => void;
+}
+
+const LocationSearch = ({ onPlaceClick }: LocationSearchProps): any => {
+    const [term, setTerm] = useState("");
+    const [places, setPlaces] = useState<Place[]>([]);
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        console.log("Need to search api...", term);
+    };
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label className='font-bold' htmlFor='term'>
+                    Search
+                </label>
+                <input
+                    className='m-2 border border-stone-500 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 px-4 py-2 w-full'
+                    id='term'
+                    type='text'
+                    value={term}
+                    onChange={(e) => setTerm(e.target.value)}
+                ></input>
+            </form>
+        </div>
+    );
 };
 
 export default LocationSearch;
