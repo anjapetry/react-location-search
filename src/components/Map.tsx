003f4f6b6b283152d0b6f9203e0e5eed803fac14
Anja Petry <anjapetry@protonmail.com>
2024-05-14 11:26:31 +0200

    feat:show and pan a map in map.tsx by implementing leaflet

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,14 +1,38 @@
 import "leaflet/dist/leaflet.css";
 import type { Place } from "../api/Place";
-import { useState, useEffect } from "react";
-import type { Map as LeafletMap } from "leaflet";
+import { useEffect, useRef } from "react";
+import { Map as LeafletMap } from "leaflet";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 interface MapProps {
     place: Place | null;
 }
 const Map = ({ place }: MapProps) => {
-    return <div>Map</div>;
+    const mapRef = useRef<LeafletMap | null>(null);
+
+    useEffect(() => {
+        if (mapRef.current && place) {
+            mapRef.current.flyTo([place.latitude, place.longitude]);
+        }
+    }, [place]);
+
+    return (
+        <MapContainer
+            ref={mapRef}
+            center={[40.7, -74]}
+            zoom={12}
+            scrollWheelZoom
+            className='w-full h-full'
+        >
+            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+            {place && (
+                <Marker
+                    position={[place.latitude, place.longitude]}
+                    title={place.name}
+                />
+            )}
+        </MapContainer>
+    );
 };
 
 export default Map;
